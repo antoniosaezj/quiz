4f97a9f008c4f3aa54de27fc5b14f3df15ca64ea
Antonio Sáez Jiménez <antonio.saezj@gmail.com>
2015-07-20 18:38:27 +0200

    Noveno Commit: Despliegue DB en Heroku.

--- models/models.js
+++ models/models.js
@@ -1,11 +1,28 @@
 var path = require('path');
 
-// Cargar Sequlize
+// Recuperamos la BD según si estamos en DESARROLLO o PRODUCCION
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user = (url[2]||null);
+var pwd = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect = (url[1]||null);
+var port = (url[5]||null);
+var host = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
+// Cargar Sequelize
 var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite
-var sequelize = new Sequelize(null,null,null,
-                      {dialect: "sqlite", storage: "quiz.sqlite"}
+var sequelize = new Sequelize(DB_name, user, pwd,
+                      {dialect: protocol,
+                       protocol: protocol,
+                       port: port,
+                       host: host,
+                       storage: storage, // Para SQLite en el entorno de DESARROLLO
+                       omitNull: true // para Posgres
+                      }
                     );
 
 // Importar la definición de la tabla Quiz de quiz.js
